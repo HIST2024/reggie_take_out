bc6cb2b504eed9466398caf365b9eb92c9b62a5b
codermao <1301878596@qq.com>
2023-04-30 22:56:28 +0800

    inital

---
+++ src/main/java/com/itheima/Main.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+/**
+ * @author codermao
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
